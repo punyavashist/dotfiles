b0a8e0957c24d091453fc373cf521927cb269a9a
Nikita Voloboev <nikita.voloboev@gmail.com>
2016-12-04 11:36:26 +0100

    :sparkles:

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -61,41 +61,6 @@ hs.urlevent.bind("someAlert", function(eventName, params)
     hs.alert.show("hey there alert")
 end)
 
--- anycomplete
-hs.hotkey.bind(cmd_alt_ctrl, "W", function()
-    local GOOGLE_ENDPOINT = 'https://suggestqueries.google.com/complete/search?client=firefox&q=%s'
-    local current = hs.application.frontmostApplication()
-
-    local chooser = hs.chooser.new(function(choosen)
-        current:activate()
-        hs.eventtap.keyStrokes(choosen.text)
-    end)
-
-    chooser:queryChangedCallback(function(string)
-        local query = urlencode.string(string)
-
-        hs.http.asyncGet(string.format(GOOGLE_ENDPOINT, query), nil, function(status, data)
-            if not data then return end
-
-            local results = hs.json.decode(data)
-
-            if not results then return end
-
-            choices = hs.fnutils.imap(results[2], function(result)
-                return {
-                    ["text"] = result,
-                }
-            end)
-
-            chooser:choices(choices)
-        end)
-    end)
-
-    chooser:searchSubText(false)
-
-    chooser:show()
-end)
-
 
 --------------------------------------------------------------------------------
 -- _meta
