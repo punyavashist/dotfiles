7108fe55e6a4d5bdfb32cda106b712c220f7dba5
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-01-04 22:20:08 +0100

    :sparkles:

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------
 -- _require
 --------------------------------------------------------------------------------
-require("hs.ipc") -- for hammerspoon cli TODO: see if I use it
+require("hs.ipc") -- hammerpoon cli
 
 
 --------------------------------------------------------------------------------
@@ -23,11 +23,57 @@ local mouseCircleTimer = nil
 --------------------------------------------------------------------------------
 -- _variables
 --------------------------------------------------------------------------------
+-- _colours
+white = hs.drawing.color.white
+black = hs.drawing.color.black
+blue = hs.drawing.color.blue
+osx_red = hs.drawing.color.osx_red
+osx_green = hs.drawing.color.osx_green
+osx_yellow = hs.drawing.color.osx_yellow
+tomato = hs.drawing.color.x11.tomato
+dodgerblue = hs.drawing.color.x11.dodgerblue
+firebrick = hs.drawing.color.x11.firebrick
+lawngreen = hs.drawing.color.x11.lawngreen
+lightseagreen = hs.drawing.color.x11.lightseagreen
+purple = hs.drawing.color.x11.purple
+royalblue = hs.drawing.color.x11.royalblue
+sandybrown = hs.drawing.color.x11.sandybrown
+black50 = {red=0,blue=0,green=0,alpha=0.5}
+darkblue = {red=24/255,blue=195/255,green=145/255,alpha=1}
 
+-- _paths
 
 --------------------------------------------------------------------------------
--- _functions
+-- _utils
 --------------------------------------------------------------------------------
+-- show my todo task in a neat window briefly
+function showTodoTask () 
+hs.alert.show( ( hs.execute("~/Documents/d.\\ app/bitbar/todo") ) )
+end
+hs.urlevent.bind("showTodoTask", showTodoTask)
+
+
+-- _display clipboard contents
+function showClipBoardContent ()
+	clipboard = hs.pasteboard.getContents()
+	hs.alert.show(clipboard)
+end
+hs.urlevent.bind("showClipBoardContent", showClipBoardContent)
+
+
+-- _wifi watcher
+wifiwatcher = hs.wifi.watcher.new(function ()
+    net = hs.wifi.currentNetwork()
+    if net==nil then
+        hs.notify.show("wifi disconnected","","","")
+    else
+        hs.notify.show("wifi connected","",net,"")
+    end
+end)
+wifiwatcher:start()
+
+
+-- _visually circle my mouse pointer
 function mouseHighlight()
     -- delete an existing highlight if it exists
     if mouseCircle then
@@ -48,94 +94,9 @@ function mouseHighlight()
     -- set a timer to delete the circle after 2 seconds
     mouseCircleTimer = hs.timer.doAfter(2, function() mouseCircle:delete() end)
 end
+hs.urlevent.bind("visuallyCircleMouse", mouseHighlight) 
 
---------------------------------------------------------------------------------
--- _utils
---------------------------------------------------------------------------------
--- show my todo task in a neat window briefly
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "I", function()
-hs.alert.show( ( hs.execute("~/Documents/d.\\ app/bitbar/todo") ) )
-end)
-
-
--- display clipboard contents
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
-	clipboard = hs.pasteboard.getContents()
-	hs.alert.show(clipboard)
-end)
-
-
--- visually circle my mouse pointer
-hs.hotkey.bind({"cmd", "shift"}, "F12", mouseHighlight) 
-
--- _wifi watcher
-wifiwatcher = hs.wifi.watcher.new(function ()
-    net = hs.wifi.currentNetwork()
-    if net==nil then
-        hs.notify.show("wifi disconnected","","","")
-    else
-        hs.notify.show("wifi connected","",net,"")
-    end
-end)
-wifiwatcher:start()
-
-local anycomplete = require "anycomplete/anycomplete"
-anycomplete.registerDefaultBindings(cmd_alt_ctrl, "W")
-
---------------------------------------------------------------------------------
--- _window management
---------------------------------------------------------------------------------
-hs.window.animationDuration = 0 
-
---------------------------------------------------------------------------------
--- _testing
---------------------------------------------------------------------------------
-hs.urlevent.bind("someAlert", function(eventName, params)
-    hs.alert.show("hey there alert")
-end)
-
--- TODO: understand this
-
-hs.hotkey.alertDuration=0
-hs.window.animationDuration = 0
-
-white = hs.drawing.color.white
-black = hs.drawing.color.black
-blue = hs.drawing.color.blue
-osx_red = hs.drawing.color.osx_red
-osx_green = hs.drawing.color.osx_green
-osx_yellow = hs.drawing.color.osx_yellow
-tomato = hs.drawing.color.x11.tomato
-dodgerblue = hs.drawing.color.x11.dodgerblue
-firebrick = hs.drawing.color.x11.firebrick
-lawngreen = hs.drawing.color.x11.lawngreen
-lightseagreen = hs.drawing.color.x11.lightseagreen
-purple = hs.drawing.color.x11.purple
-royalblue = hs.drawing.color.x11.royalblue
-sandybrown = hs.drawing.color.x11.sandybrown
-black50 = {red=0,blue=0,green=0,alpha=0.5}
-darkblue = {red=24/255,blue=195/255,green=145/255,alpha=1}
-
-privatepath = hs.fs.pathToAbsolute(hs.fs.currentDir()..'/private')
-if privatepath == nil then
-    hs.fs.mkdir(hs.fs.currentDir()..'/private')
-end
-privateconf = hs.fs.pathToAbsolute(hs.fs.currentDir()..'/private/awesomeconfig.lua')
-if privateconf ~= nil then
-    require('private/awesomeconfig')
-end
-
-hsreload_keys = hsreload_keys or {{"cmd", "shift", "ctrl"}, "R"}
-if string.len(hsreload_keys[2]) > 0 then
-    hs.hotkey.bind(hsreload_keys[1], hsreload_keys[2], "Reload Configuration", function() hs.reload() end)
-end
-
-lockscreen_keys = lockscreen_keys or {{"cmd", "shift", "ctrl"}, "L"}
-if string.len(lockscreen_keys[2]) > 0 then
-    hs.hotkey.bind(lockscreen_keys[1], lockscreen_keys[2],"Lock Screen", function() hs.caffeinate.lockScreen() end)
-end
-
-function show_time()
+function showTime()
     if not time_draw then
         local mainScreen = hs.screen.mainScreen()
         local mainRes = mainScreen:fullFrame()
@@ -150,98 +111,24 @@ function show_time()
         time_draw=nil
     end
 end
+hs.urlevent.bind("showTime", showTime) 
 
-function showavailableHotkey()
-    if not hotkeytext then
-        local hotkey_list=hs.hotkey.getHotkeys()
-        local mainScreen = hs.screen.mainScreen()
-        local mainRes = mainScreen:fullFrame()
-        local hkbgrect = hs.geometry.rect(mainRes.w/5,mainRes.h/5,mainRes.w/5*3,mainRes.h/5*3)
-        hotkeybg = hs.drawing.rectangle(hkbgrect)
-        -- hotkeybg:setStroke(false)
-        hotkeybg:setFillColor({red=0,blue=0,green=0,alpha=0.5})
-        hotkeybg:setRoundedRectRadii(10,10)
-        hotkeybg:setLevel(hs.drawing.windowLevels.modalPanel)
-        hotkeybg:setBehavior(hs.drawing.windowBehaviors.stationary)
-        local hktextrect = hs.geometry.rect(hkbgrect.x+40,hkbgrect.y+30,hkbgrect.w-80,hkbgrect.h-60)
-        hotkeytext = hs.drawing.text(hktextrect,"")
-        hotkeytext:setLevel(hs.drawing.windowLevels.modalPanel)
-        hotkeytext:setBehavior(hs.drawing.windowBehaviors.stationary)
-        hotkeytext:setClickCallback(nil,function() hotkeytext:delete() hotkeytext=nil hotkeybg:delete() hotkeybg=nil end)
-        hotkey_filtered = {}
-        for i=1,#hotkey_list do
-            if hotkey_list[i].idx ~= hotkey_list[i].msg then
-                table.insert(hotkey_filtered,hotkey_list[i])
-            end
-        end
-        local availablelen = 70
-        local hkstr = ''
-        for i=2,#hotkey_filtered,2 do
-            local tmpstr = hotkey_filtered[i-1].msg .. hotkey_filtered[i].msg
-            if string.len(tmpstr)<= availablelen then
-                local tofilllen = availablelen-string.len(hotkey_filtered[i-1].msg)
-                hkstr = hkstr .. hotkey_filtered[i-1].msg .. string.format('%'..tofilllen..'s',hotkey_filtered[i].msg) .. '\n'
-            else
-                hkstr = hkstr .. hotkey_filtered[i-1].msg .. '\n' .. hotkey_filtered[i].msg .. '\n'
-            end
-        end
-        if math.fmod(#hotkey_filtered,2) == 1 then hkstr = hkstr .. hotkey_filtered[#hotkey_filtered].msg end
-        local hkstr_styled = hs.styledtext.new(hkstr, {font={name="Courier-Bold",size=16}, color=white, paragraphStyle={lineSpacing=12.0,lineBreak='truncateMiddle'}})
-        hotkeytext:setStyledText(hkstr_styled)
-        hotkeybg:show()
-        hotkeytext:show()
-    else
-        hotkeytext:delete()
-        hotkeytext=nil
-        hotkeybg:delete()
-        hotkeybg=nil
-    end
-end
+--------------------------------------------------------------------------------
+-- _window management
+--------------------------------------------------------------------------------
+hs.window.animationDuration = 0 
+hs.hotkey.alertDuration=0
 
-modal_list = {}
 
-function modal_stat(modal, color)
-    if not modal_show then
-        local mainScreen = hs.screen.mainScreen()
-        local mainRes = mainScreen:fullFrame()
-        local modal_bg_rect = hs.geometry.rect(mainRes.w-170,mainRes.h-24,170,19)
-        local modal_text_rect = hs.geometry.rect(mainRes.w-170,mainRes.h-24,170,19)
-        modal_bg = hs.drawing.rectangle(modal_bg_rect)
-        modal_bg:setStroke(false)
-        modal_bg:setFillColor(black)
-        modal_bg:setRoundedRectRadii(2,2)
-        modal_bg:setLevel(hs.drawing.windowLevels.status)
-        modal_bg:setBehavior(hs.drawing.windowBehaviors.canJoinAllSpaces+hs.drawing.windowBehaviors.stationary)
-        local styledText = hs.styledtext.new("init...",{font={size=14.0,color=white},paragraphStyle={alignment="center"}})
-        modal_show = hs.drawing.text(modal_text_rect,styledText)
-        modal_show:setLevel(hs.drawing.windowLevels.status)
-        modal_show:setBehavior(hs.drawing.windowBehaviors.canJoinAllSpaces+hs.drawing.windowBehaviors.stationary)
-    end
-    modal_bg:show()
-    modal_bg:setFillColor(color)
-    modal_show:show()
-    modal_text = string.upper(modal .. ' mode')
-    modal_show:setText(" ")
-end
+--------------------------------------------------------------------------------
+-- _testing
+--------------------------------------------------------------------------------
+hs.urlevent.bind("someAlert", function(eventName, params)
+    hs.alert.show("hey there alert")
+end)
 
-function exit_others(except)
-    for i = 1, #modal_list do
-        if modal_list[i] ~= except then
-            modal_list[i]:exit()
-        end
-    end
-end
-
-function check_idle()
-    if modal_text == 'DOCK MODE' then
-        if idle_to_which == "netspeed" and netspeedM then
-            netspeedM:enter()
-        elseif idle_to_which == "hide" then
-            modal_show:hide()
-            modal_bg:hide()
-        end
-    end
-end
+local anycomplete = require "anycomplete/anycomplete"
+anycomplete.registerDefaultBindings(cmd_alt_ctrl, "W")
 
 function resize_win(direction)
     local win = hs.window.focusedWindow()
@@ -274,65 +161,23 @@ function resize_win(direction)
         if direction == "mdown" then f.y = f.y+steph end
         win:setFrame(f)
     else
-        hs.alert.show("No focused window!")
+        hs.alert.show("no focused window!")
     end
 end
 
-resizeextra_lefthalf_keys = resizeextra_lefthalf_keys or {{"cmd", "alt"}, "left"}
-if string.len(resizeextra_lefthalf_keys[2]) > 0 then
-    hs.hotkey.bind(resizeextra_lefthalf_keys[1], resizeextra_lefthalf_keys[2], "Lefthalf of Screen", function() resize_win('halfleft') end)
+function test () 
+    resize_win('left')
 end
-resizeextra_righthalf_keys = resizeextra_righthalf_keys or {{"cmd", "alt"}, "right"}
-if string.len(resizeextra_righthalf_keys[2]) > 0 then
-    hs.hotkey.bind(resizeextra_righthalf_keys[1], resizeextra_righthalf_keys[2], "Righthalf of Screen", function() resize_win('halfright') end)
-end
-resizeextra_fullscreen_keys = resizeextra_fullscreen_keys or {{"cmd", "alt"}, "up"}
-if string.len(resizeextra_fullscreen_keys[2]) > 0 then
-    hs.hotkey.bind(resizeextra_fullscreen_keys[1], resizeextra_fullscreen_keys[2], "Fullscreen", function() resize_win('fullscreen') end)
-end
-resizeextra_fcenter_keys = resizeextra_fcenter_keys or {{"cmd", "alt"}, "down"}
-if string.len(resizeextra_fcenter_keys[2]) > 0 then
-    hs.hotkey.bind(resizeextra_fcenter_keys[1], resizeextra_fcenter_keys[2], "Resize & Center", function() resize_win('fcenter') end)
-end
-resizeextra_center_keys = resizeextra_center_keys or {{"cmd", "alt"}, "return"}
-if string.len(resizeextra_center_keys[2]) > 0 then
-    hs.hotkey.bind(resizeextra_center_keys[1], resizeextra_center_keys[2], "Center Window", function() resize_win('center') end)
-end
-
-if not module_list then
-    module_list = {
-        "basicmode",
-        "widgets/netspeed",
-        "widgets/calendar",
-        "widgets/analogclock",
-        "modes/indicator",
-        "modes/clipshow",
-        "modes/cheatsheet",
-        "modes/hsearch",
-    }
-end
-
-for i=1,#module_list do
-    require(module_list[i])
-end
-
-if #modal_list > 0 then require("modalmgr") end
+hs.urlevent.bind("tester", test)
 
 --------------------------------------------------------------------------------
 -- _meta 
 --------------------------------------------------------------------------------
 hs.hotkey.bind("alt", "M", hs.toggleConsole)
 
+hs.urlevent.bind("reloadConfig", hs.reload)
 
--- needed for cli utility I use to reload config
-require("hs.ipc")
-
--- reload config from hotkey
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
-  hs.reload()
-end)
-
--- called on every config reload to notify if it was reloaded
+-- _notify that config is reloaded
 hs.alert.show("config loaded")
 
 
