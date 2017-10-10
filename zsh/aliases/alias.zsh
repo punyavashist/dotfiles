fba07a14e2f07592f9529efaa6545798f7290f87
Nikita Voloboev <nikita.voloboev@icloud.com>
2017-10-10 21:33:44 +0200

    ✨

--- zsh/aliases/alias.zsh
+++ zsh/aliases/alias.zsh
@@ -321,48 +321,24 @@ alias drb='cd ~/code/ruby'
 alias dpp='cd ~/code/python'
 alias dlove='cd ~/code/lua/love'
 alias djs='cd ~/code/js'
-#alias da='cd ~/code' TODO: change
 alias dgo='cd ~/code/go'
 alias dsj='cd ~/code/solutions/java'
 alias dss='cd ~/oss/nikivi/solutions'
 alias di='cd ~/ideas'
 
-# _xcode - swift
+# _xcode
 alias dx='cd ~/xcode'
-alias dxi='cd ~/xcode/projects/ios'
-alias dxm='cd ~/xcode/projects/macos'
+alias dxi='cd ~/xcode/ios'
+alias dxm='cd ~/xcode/macos'
 
 # _web
-alias dwp='cd ~/web/projects'
 alias dw='cd ~/web'
-
-# _research
-alias dr='cd ~/research'
-alias dai='cd ~/research/ai'
-alias dapp='cd ~/research/apps'
-alias dcr='cd ~/research/crypto'
-alias ddot='cd ~/research/dots'
-alias drg='cd ~/research/general'
-alias dreact='cd ~/research/react'
-alias dxr='cd ~/xcode/research'
-alias dres='cd ~/research/resources'
-
-# _research code
-alias dbr='cd ~/code/bash/research'
-alias dcr='cd ~/code/c/research'
-alias djr='cd ~/code/java/research'
-alias dpr="cd ~/code/python/research"
-alias dgr='cd ~/code/go/research'
-alias djsr='cd ~/code/js/research'
-alias docr='cd ~code/objc/research'
-alias dwr='cd ~/web/research'
 
 # _oss
 alias ds='cd ~/oss/'
 alias dsn='cd ~/oss/nikivi'
-alias dsr='cd ~/oss/research'
-alias dsf='cd ~/oss/forks'
-alias d:='cd ~/oss/learn-anything'
+alias dt='cd ~/oss/learn-anything'
+alias d:='cd ~/oss/nikivi/code'
 
 # _macos
 alias de='cd ~/Desktop'
@@ -372,12 +348,10 @@ alias dnm='cd /usr/local/lib/node_modules'
 alias dbb='cd /usr/local/bin'
 alias dcl='cd /usr/local/Cellar'
 alias drp='cd ~/Dropbox'
-alias dbl='cd ~/oss/nikivi/hugo/content/post'
 
 # _config
 alias d..='cd ~/.config'
-alias dbi='cd ~/.dotfiles/bin/internets'
-alias dv='cd ~/.dotfiles/vim'
+alias dv='cd ~/.dotfiles/nvim'
 alias da='cd ~/.dotfiles'
 alias dsk='cd ~/Library/Application\ Support/Karabiner'
 alias db='cd ~/.dotfiles/bin'
